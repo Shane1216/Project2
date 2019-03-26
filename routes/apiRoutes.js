d07d0198e1a2a6685956961f6d08c5a5ec87f322
doctoron <ron.antonio@me.com>
2019-03-26 12:31:35 -0400

    routes table

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,28 +1,3 @@
-var db = require("../models");
-
-module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
-
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-};
-
 // *********************************************************************************
 // api-routes.js - this file offers a set of routes for displaying and saving data to the db
 // *********************************************************************************
@@ -31,64 +6,28 @@ module.exports = function(app) {
 // =============================================================
 
 // Requiring our models
-var db = require("../models");
+let db = require("../models");
 
-// Routes
-// =============================================================
 module.exports = function(app) {
-
-  // GET route for getting all of the todos
-  app.get("/api/todos", function(req, res) {
-    // findAll returns all entries for a table when used with no options
-    db.Todo.findAll({}).then(function(dbTodo) {
-      // We have access to the todos as an argument inside of the callback function
-      res.json(dbTodo);
+  // Get all examples
+  app.get("/api/rail_stop", function(req, res) {
+    db.rail_stop.findAll({}).then(function(dbRail_stop) {
+      res.json(dbExamples);
     });
   });
 
-  // POST route for saving a new todo
-  app.post("/api/todos", function(req, res) {
-    // create takes an argument of an object describing the item we want to
-    // insert into our table. In this case we just we pass in an object with a text
-    // and complete property
-    db.Todo.create({
-      text: req.body.text,
-      complete: req.body.complete
-    }).then(function(dbTodo) {
-      // We have access to the new todo as an argument inside of the callback function
-      res.json(dbTodo);
+  // Create a new example
+  app.post("/api/rail_stop", function(req, res) {
+    db.rail_stop.create(req.body).then(function(dbRail_stop) {
+      res.json(dbExample);
     });
   });
 
-  // DELETE route for deleting todos. We can get the id of the todo to be deleted from
-  // req.params.id
-  app.delete("/api/todos/:id", function(req, res) {
-    // We just have to specify which todo we want to destroy with "where"
-    db.Todo.destroy({
-      where: {
-        id: req.params.id
-      }
-    }).then(function(dbTodo) {
-      res.json(dbTodo);
-    });
-
-  });
-
-  // PUT route for updating todos. We can get the updated todo data from req.body
-  app.put("/api/todos", function(req, res) {
-    // Update takes in an object describing the properties we want to update, and
-    // we use where to describe which objects we want to update
-    db.Todo.update({
-      text: req.body.text,
-      complete: req.body.complete
-    }, {
-      where: {
-        id: req.body.id
-      }
-    }).then(function(dbTodo) {
-      res.json(dbTodo);
+  // Delete an example by id
+  app.delete("/api/rail_stop/:id", function(req, res) {
+    db.rail_stop.destroy({ where: { id: req.params.id } }).then(function(dbRail_stop) {
+      res.json(dbExample);
     });
   });
-
 };
 
