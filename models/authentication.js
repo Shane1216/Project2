71e684ae022ffbef91084757b17a53f6cf8d563a
moonlagoon <courtneyroutsong@gmail.com>
2019-03-26 20:54:01 -0400

    added handbars page tickets and login. new models authentication

---
+++ models/authentication.js
@@ -0,0 +1,15 @@
+var passport = require("passport");
+
+module.exports = function(sequelize, DataTypes) {
+    var User = sequelize.define("User", {
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      }
+    });
+    return User;
+   };
