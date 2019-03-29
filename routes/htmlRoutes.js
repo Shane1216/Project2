153e2a045306ffe28740f25054966633068bbf75
reubenwalker64 <rwalker@mobile-atom.com>
2019-03-28 20:29:24 -0400

    Thursday Night

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,19 +3,66 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    db.Sunrail_stops.findAll({}).then(function(dbSunrail) {
       res.render("index", {
         msg: "Welcome!",
-        examples: dbExamples
+        trains: dbSunrail
+      });
+    });
+  });
+
+  // Load Northbound page
+  app.get("/northbound", function(req, res) {
+    db.Sunrail_stops.findAll({}).then(function(dbSunrail) {
+      res.render("northbound", {
+        trains: dbSunrail
+      });
+    });
+  });
+
+  // Load Southhbound page
+  app.get("/southbound", function(req, res) {
+    db.Sunrail_stops.findAll({}).then(function(dbSunrail) {
+      res.render("southbound", {
+        trains: dbSunrail
+      });
+    });
+  });
+
+  // Load Map page
+  app.get("/map", function(req, res) {
+    db.Sunrail_stops.findAll({}).then(function(dbSunrail) {
+      res.render("map", {
+        trains: dbSunrail
+      });
+    });
+  });
+
+  // Load Tickets page
+  app.get("/tickets", function(req, res) {
+    db.Sunrail_stops.findAll({}).then(function(dbSunrail) {
+      res.render("tickets", {
+        trains: dbSunrail
+      });
+    });
+  });
+
+  app.get("/login", function(req, res) {
+    db.Sunrail_stops.findAll({}).then(function(dbSunrail) {
+      res.render("login", {
+        trains: dbSunrail
       });
     });
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.get("/stop/:id", function(req, res) {
+    db.Sunrail_stops.findOne({ where: { id: req.params.id } }).then(function(
+      dbSunrail
+    ) {
+      console.log(dbSunrail);
       res.render("example", {
-        example: dbExample
+        trains: dbSunrail
       });
     });
   });
